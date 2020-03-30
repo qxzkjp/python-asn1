bc1bf3799d09c9c484028b94b25fa3669846cc28
Sebastien Andrivet <sebastien@andrivet.com>
2020-03-30 15:08:33 +0200

    Prepare version 2.3 with Python 3.7 and 3.8 support

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ if version_info[0] < 3:
 
 setup(
     name='asn1',
-    version='2.2.0',
+    version='2.3.0',
     license='BSD',
     description='Python-ASN1 is a simple ASN.1 encoder and decoder for Python 2.6+ and 3.3+.',
     long_description='%s\n%s' % (
@@ -53,13 +53,14 @@ setup(
         'Operating System :: POSIX',
         'Operating System :: Microsoft :: Windows',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Utilities',
